281b81128ddfad895056d96fd34b7775ddd1d8f2
tamayama <h.tamayama.sys24@morijyobi.ac.jp>
2024-11-19 13:27:11 +0900

    全修正

--- source/janken.py
+++ source/janken.py
@@ -3,33 +3,24 @@ import computer
 import janken_judge
 
 def janken_main():
+    janken_hand = ['グー', 'チョキ', 'パー']
     count = 0
+   
     playercount = 0
     computercount = 0
-    playerhand = player.pon()
-    computerhand = computer.pon()
     
     while count < 3:
-        if(playerhand == 1 and computerhand == 2 or playerhand == 2 and computerhand == 3 or playerhand == 3 and computerhand == 1):
-            print(F"あなたの手:{player}")
-            print(F"相手の手:{computer}")
-            print(janken_judge.judge())
-            count += 1
-            playercount += 1
-        elif(playerhand == 1 and computerhand == 1 or playerhand == 2 and computerhand == 2 or playerhand == 3 and computerhand == 3):
-            print(F"あなたの手:{player}")
-            print(F"相手の手:{computer}")
-            print(janken_judge.judge())
-        else:
-            print(F"あなたの手:{player}")
-            print(F"相手の手:{computer}")
-            print(janken_judge.judge())
-            count += 1
-            computercount += 1
-            
-            
+        playerhand = player.pon()
+        computerhand = computer.pon()
+        janken_judge.judge(playerhand, computerhand)
+        count += 1
+ 
+        
+    result = janken_judge.judge(playerhand, computerhand)
+    if result:
+        playercount += 1
     print(F"あなた:{playercount}勝")
-    print(F"あなた:{computercount}勝")
+    print(F"コンピュータ:{3 - playercount}勝")
     if(playercount >= 2):
         print("あなたの総合勝利です！")
     else:
--- source/player.py
+++ source/player.py
@@ -1,4 +1,4 @@
 
 def pon():
-    a = int(input())
+    a = int(input("手を入力してください。\n1.グー 2.チョキ 3.パー"))
     return a
